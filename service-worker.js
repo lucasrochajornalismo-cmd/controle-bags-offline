60175ea50ec36adbfab594c652d30b86110880e4
lucasrochajornalismo-cmd <lucasrochajornalismo@gmail.com>
2026-08-18 02:43:44 +0000

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -4,7 +4,7 @@ const APP_SHELL = [
   './index.html',
   './manifest.webmanifest',
   './icons/icon-192.png',
-  './icons/icon-512.png'
+  './icon-512.png'
 ];
 
 self.addEventListener('install', event => {
